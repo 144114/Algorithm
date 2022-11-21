2811d118e4e5c1c13bf51722841eaef39c64173c
144114 <118462883+144114@users.noreply.github.com>
2022-11-22 06:56:18 +0700

    Final

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ from seat import *
 
 ticket_number = 0
 df = pd.read_excel("USDOT_Consumer_Airfare_2007-2017-1.xlsx")
-df = df.head(10000)
+df = df.head(1000)
 
 def Data_creation(number_of_data=30,df=df):
     Sample_data = df[["Year","City1","City2","Average Fare"]].sample(number_of_data)
@@ -19,7 +19,7 @@ def Data_creation(number_of_data=30,df=df):
 
     for i in range(0,number_of_data):
         hold_list = seat.price_config(Sample_data["Average Fare"].iloc[i])
-        Sample_data["Average Fare"].iloc[i] = hold_list[0]
+        Sample_data["Average Fare"].iloc[i] = int(hold_list[0])
         Sample_data["Classes"].iloc[i] = hold_list[1]
         Sample_data["Window"].iloc[i] = hold_list[2]
         Sample_data["Date"].iloc[i] = seat.random_date(Sample_data["Year"].iloc[i])
@@ -31,4 +31,32 @@ def Data_creation(number_of_data=30,df=df):
     Sample_data = Sample_data.drop(["Year"],axis=1)
     return Sample_data
 
-Sample_data = Data_creation()
+ticket_list = []
+
+def Data_creation_randomticketnumber(number_of_data=30,df=df,max_ticket=1000):
+    global ticket_list
+    Sample_data = df[["Year","City1","City2","Average Fare"]].sample(number_of_data)
+    seat = Seats()
+    Sample_data["Classes"] = [i for i in range(number_of_data)]
+    Sample_data["Window"] = [i for i in range(number_of_data)]
+    Sample_data["Seat Number"] = [i for i in range(number_of_data)]
+    Sample_data["Date"] = [i for i in range(number_of_data)]
+    Sample_data["Time"] = [i for i in range(number_of_data)]
+    Sample_data["Ticket Number"] = [i for i in range(number_of_data)]
+
+    for i in range(0,number_of_data):
+        hold_list = seat.price_config(Sample_data["Average Fare"].iloc[i])
+        Sample_data["Average Fare"].iloc[i] = hold_list[0]
+        Sample_data["Classes"].iloc[i] = hold_list[1]
+        Sample_data["Window"].iloc[i] = hold_list[2]
+        Sample_data["Date"].iloc[i] = seat.random_date(Sample_data["Year"].iloc[i])
+        Sample_data["Time"].iloc[i] = seat.random_time()
+        Sample_data["Seat Number"].iloc[i] = seat.seat_number_config()
+        ticket = random.randint(1,max_ticket)
+        while ticket in ticket_list:
+            ticket = random.randint(1,max_ticket)
+        ticket_list.append(ticket)
+        Sample_data["Ticket Number"].iloc[i] = ticket_list[i]
+        
+    Sample_data = Sample_data.drop(["Year"],axis=1)
+    return Sample_data 
